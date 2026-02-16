a7263f5657fb154c6f388d946885531a51b72bc0
SAKSHAM JOSHI <109752898+saksham-joshi@users.noreply.github.com>
2026-02-16 08:01:48 +0530

    Prettified

--- site/lib/keys.tsx
+++ site/lib/keys.tsx
@@ -1,22 +1,23 @@
 export const JSON_KEYS = {
 
     // request keys
-    apiKey : "api-key",
-    comment : "comment",
-    commentList : "comment-list",
+    apiKey: "api-key",
+    comment: "comment",
+    commentList: "comment-list",
 
     // response keys
-    positiveScore : "pos-score",
-    negativeScore : "neg-score",
-    polarityScore : "pol-score",
-    sentiment : "senti",
-    positiveWords : "pos-words",
-    negativeWords : "neg-words",
-    unidentifiedWords : "uniden-words",
-    wordCloud : "word-cloud",
-    keywords : "keywords",
-    
+    positiveScore: "pos-score",
+    negativeScore: "neg-score",
+    polarityScore: "pol-score",
+    sentiment: "senti",
+    positiveWords: "pos-words",
+    negativeWords: "neg-words",
+    unidentifiedWords: "uniden-words",
+    wordCloud: "word-cloud",
+    keywords: "keywords",
+    error: "error",
+
     // for tokens
-    tokenCount : "tokenCount",
-    password : "password"
+    tokenCount: "token-count",
+    password: "password"
 } as const;
